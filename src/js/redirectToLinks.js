4363b776d963a7e2c10d6165d2207914c1009c91
José Lucas Mota <onlylussc4@gmail.com>
2025-11-04 18:04:51 -0300

    Adição de um carrossel na área de suporte com SwiperJS

--- src/js/redirectToLinks.js
+++ src/js/redirectToLinks.js
@@ -2,6 +2,57 @@ document.addEventListener("DOMContentLoaded", () => {
   const doc = document.querySelector("#redirect_to_documentation");
   
   doc.addEventListener("click", () => {
-    window.open("https://www.instagram.com/mota.lsx/", "_blank");
+    window.open("https://www.instagram.com/idevallhalla/", "_blank");
+  });
+
+  const ig_laura = document.querySelector("#ig-laura");
+  const ig_melyssa = document.querySelector("#ig-melyssa");
+  const ig_caua = document.querySelector("#ig-caua");
+  const ig_lindem = document.querySelector("#ig-lindem");
+  const ig_heitorf = document.querySelector("#ig-heitorf");
+  const ig_miguel = document.querySelector("#ig-miguel");
+  const ig_mota = document.querySelector("#ig-mota");
+  const ig_lima = document.querySelector("#ig-lima");
+  const ig_samuel = document.querySelector("#ig-samuel");
+  const ig_rafael = document.querySelector("#ig-rafael");
+
+  ig_laura.addEventListener("click", () => {
+    window.open("https://www.instagram.com/morcegxit/", "_blank");
+  });
+
+  ig_melyssa.addEventListener("click", () => {
+    window.open("https://www.instagram.com/mellfariass_/", "_blank");
+  });
+
+  ig_caua.addEventListener("click", () => {
+    window.open("https://www.instagram.com/cauahenriquef.a/", "_blank");
+  });
+
+  ig_lindem.addEventListener("click", () => {
+    window.open("https://www.instagram.com/berg0_0_/", "_blank");
+  });
+
+  ig_heitorf.addEventListener("click", () => {
+    window.open("https://www.instagram.com/heitor.flv/", "_blank");
+  });
+
+  ig_miguel.addEventListener("click", () => {
+    window.open("https://www.instagram.com/miguel_do_pastel06/", "_blank");
+  });
+
+  ig_mota.addEventListener("click", () => {
+    window.open("https://www.instagram.com/mota.lsx", "_blank");
+  });
+
+  ig_lima.addEventListener("click", () => {
+    window.open("https://www.instagram.com/llimazw7/", "_blank");
+  });
+
+  ig_samuel.addEventListener("click", () => {
+    window.open("https://www.instagram.com/idevallhalla/", "_blank");
+  });
+
+  ig_rafael.addEventListener("click", () => {
+    window.open("https://www.instagram.com/idevallhalla/", "_blank");
   });
 });
